2030245bdd3fcaf5ca736b5c20b939d776cadeb8
ApiwatMekwan <chokeapiwat@gmail.com>
2019-03-16 20:01:27 +0700

    third commit : edit gitignore and add config/db,php

---
+++ config/db.php
@@ -0,0 +1,28 @@
+<?php
+
+
+//   return [
+//   'class' => 'yii\db\Connection',
+//   'dsn' => 'mysql:host=10.199.66.53;dbname=eoffice_central',
+//   'username' => 'eoffice_central',
+//    'password' => 'eoffice_central',
+//   'charset' => 'utf8',
+//];
+
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=projectsoften',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
+
+
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=127.0.0.1;dbname=eoffice_main_new',
+//    'username' => 'root',
+//    'password' => '',
+//    'charset' => 'utf8',
+//];
