1181cc048cdc70a91607429d43c0915aea2cb7d2
ApiwatMekwan <chokeapiwat@gmail.com>
2019-03-18 20:29:20 +0700

    commit six : edit file subject\_form.php button save -> บันทึก

--- views/subject/_form.php
+++ views/subject/_form.php
@@ -27,7 +27,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'cStatus')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('บันทึก', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
