d343b7a35c79958600296eb2b43928cbc984e637
Benz Boonsaree <33254030+BenzPowder@users.noreply.github.com>
2019-03-18 23:36:32 +0700

    Revert "commit nine : edit site/index"
    This reverts commit 491fbd4c4fe816ab7b967aa0b2922bf19995d8c0.

--- views/site/index.php
+++ views/site/index.php
@@ -2,31 +2,46 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'ระบบเช็คชื่อนักศึกษา';
+$this->title = 'My Yii Application';
 ?>
 <head>
 </head>
 <div class="site-index">
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand">ระบบเช็คชื่อนักศึกษา</a>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">ระบบเช็คชื่อนักศึกษา</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="/projectsoften/web/student/index">นักศึกษา <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="/projectsoften/web/subject/index">วิชา</a>
+        <a class="nav-item nav-link" href="/projectsoften/web/teacher/index">อาจารย์</a>
+        <a class="nav-item nav-link" href="/projectsoften/web/ta/index">ผู้ช่วยอาจารย์</a>
       </div>
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="/projectsoften/web/site/index">หน้าแรก</a></li>
-        <li><a href="/projectsoften/web/student/index">นักศึกษา</a></li>
-        <li><a href="/projectsoften/web/subject/index">วิชา</a></li>
-        <li><a href="/projectsoften/web/teacher/index">อาจารย์</a></li>
-        <li><a href="/projectsoften/web/ta/index">ผู้ช่วยอาจารย์</a></li>
-        <li><a href="/projectsoften/web/data-ta/create">กำหนดผู้ช่วยอาจารย์ประจำวิชา</a></li>
+    </div>
+  </nav>
+
+  <nav class="nav-extended">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
       </ul>
       </div>
     </nav>
-    <div class="container">
+    <!-- <div class="container">
       <ol class="breadcrumb">
           <li><a href="index">ระบบเช็คชื่อนักศึกษา</a></li>
           <li class="active">หน้าแรก</li>
       </ol>
-    </div>
+    </div> -->
 
+  <div id="test1" class="col s12">Test 1</div>
+  <div id="test2" class="col s12">Test 2</div>
+  <div id="test3" class="col s12">Test 3</div>
+  <div id="test4" class="col s12">Test 4</div>
 </div>
